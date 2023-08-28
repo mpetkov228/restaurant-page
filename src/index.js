86270c774dcb33617e310392d4555c3a2df05f62
MihailPetkov96 <mpetkov228@gmail.com>
2023-08-28 17:17:40 +0300

    Separate home page content into module

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
+import { loadHome } from './home.js';
+
 const contentDiv = document.getElementById('content');
 
-const h1 = document.createElement('h1');
-h1.textContent = 'MY RESTAURANT';
-
-const img = document.createElement('img');
-img.src = '../pasta.jpg';
-
-const p = document.createElement('p');
-p.textContent = 'Welcome to my restaurant. We\'ve just opened and are serving all kinds of delicious food. Come on in and have a lovely time.'
-
-contentDiv.appendChild(h1);
-contentDiv.appendChild(img);
-contentDiv.appendChild(p);
+loadHome(contentDiv);
