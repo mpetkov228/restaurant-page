5a62cbb1828d31ef8571c07799baf287deac061a
MihailPetkov96 <mpetkov228@gmail.com>
2023-08-29 16:28:51 +0300

    Add background image

--- src/home.js
+++ src/home.js
@@ -4,13 +4,9 @@ export function loadHome(contentDiv) {
     const h1 = document.createElement('h1');
     h1.textContent = 'MY RESTAURANT';
 
-    const img = document.createElement('img');
-    img.src = '../pasta.jpg';
-
     const p = document.createElement('p');
     p.textContent = 'Welcome to my restaurant. We\'ve just opened and are serving all kinds of delicious food. Come on in and have a lovely time.'
 
     contentDiv.appendChild(h1);
-    contentDiv.appendChild(img);
     contentDiv.appendChild(p);
 }
